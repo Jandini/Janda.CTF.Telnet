c6994c953a803ec9de1b4d130251e24d7a05233f
Matt Janda <matt.janda.kingston@gmail.com>
2021-10-18 01:16:48 +0100

    feat: Remove ctf extensions dependency

---
+++ src/Janda.CTF.Telnet/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Janda.CTF
+{
+    static class StringExtensions
+    {
+        internal static string ToPlural(this string value, int count)
+        {
+            if (count > 1 || count == 0)
+                return value + "s";
+
+            return value;
+        }
+    }
+}
